56d25740a0f1c6c719ed5a7ce02025348463c403
yokina <--->
2023-07-07 16:43:58 +0300

    fix

--- src/main/java/com/example/ThymeleafSprinBootCrud/ThymeleafSpringBootCrudApplication.java
+++ src/main/java/com/example/ThymeleafSprinBootCrud/ThymeleafSpringBootCrudApplication.java
@@ -29,21 +29,21 @@ public class ThymeleafSpringBootCrudApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        userRepository.deleteAll();
-
-        User user1 = new User("ivan", 21, "1238125673");
-        User user2 = new User("wouaeh", 43, "123554645");
-        User user3 = new User("waoueh", 56, "12334534");
-        User user4 = new User("grecj", 23, "123123543");
-        User user5 = new User("roman", 65, "10238190238");
-
-        userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user3);
-        userRepository.save(user4);
-        userRepository.save(user5);
-
-        System.out.println("--------------------------------");
-        userRepository.findAll().forEach(System.out::println);
+//        userRepository.deleteAll();
+//
+//        User user1 = new User("ivan", 21, "1238125673");
+//        User user2 = new User("wouaeh", 43, "123554645");
+//        User user3 = new User("waoueh", 56, "12334534");
+//        User user4 = new User("grecj", 23, "123123543");
+//        User user5 = new User("roman", 65, "10238190238");
+//
+//        userRepository.save(user1);
+//        userRepository.save(user2);
+//        userRepository.save(user3);
+//        userRepository.save(user4);
+//        userRepository.save(user5);
+//
+//        System.out.println("--------------------------------");
+//        userRepository.findAll().forEach(System.out::println);
     }
 }
